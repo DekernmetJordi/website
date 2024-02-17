69ed07ef91ca7ad29678b7bf016b984ec37e71d8
Dekernmetjordi <info@dekernmetjordi.com>
2024-02-17 09:44:08 +0100

    Sitemap goedgezet

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
-  site: "https://dekernmetjordi"
+  site: "https://dekernmetjordi.com"
 });
