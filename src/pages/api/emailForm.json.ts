7a9febcd8b18a658de5ea37ecf13f475e075adb7
Dekernmetjordi <info@dekernmetjordi.com>
2024-05-05 13:40:39 +0200

    nieuwsbrief toegevoegd

--- src/pages/api/emailForm.json.ts
+++ src/pages/api/emailForm.json.ts
@@ -26,18 +26,9 @@ export const POST: APIRoute = async ({ request }) => {
     .createOrUpdate(params)
     .then((response) => {
       console.log(response.data);
-      return new Response(
-        JSON.stringify({
-          message: response.data,
-        })
-      );
     })
     .catch((error) => {
       if (error.response) console.log(error.response.data);
-      return new Response(
-        JSON.stringify({
-          message: error.response.data,
-        }))
     });
 
   // const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
@@ -49,21 +40,21 @@ export const POST: APIRoute = async ({ request }) => {
   //     Authentication: `Bearer ${key}`,
   //   },
 
-  // const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-  //   method: "POST",
-  //   body: JSON.stringify(data),
-  //   headers: {
-  //     "Content-type": "application/json; charset=UTF-8",
-  //     // "Accept": "application/json",
-  //     Authentication: `Bearer ${key}`,
-  //   },
-  // });
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      // "Accept": "application/json",
+      Authentication: `Bearer ${key}`,
+    },
+  });
 
-  // const output = await res.json();
-  // console.log("output is: " + JSON.stringify(output));
-  // return new Response(
-  //   JSON.stringify({
-  //     message: output.id ? "success" : "failure",
-  //   })
-  // );
+  const output = await res.json();
+  console.log("output is: " + JSON.stringify(output));
+  return new Response(
+    JSON.stringify({
+      message: output.id ? "success" : "failure",
+    })
+  );
 };
