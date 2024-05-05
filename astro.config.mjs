ffb563aaf1064ed54c786d6dc62338b25f637406
Dekernmetjordi <info@dekernmetjordi.com>
2024-05-05 11:22:53 +0200

    nieuwsbrief toegevoegd

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,16 @@ import partytown from "@astrojs/partytown";
 
 // import compress from "astro-compress";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap({
     filter: page => page !== "https://dekernmetjordi.com/the-art-of-music-production" && page !== "https://dekernmetjordi.com/the-importance-of-audio-quality" && page !== "https://dekernmetjordi.com/tune-your-snare-drum" && page !== "https://dekernmetjordi.com/gear-is-insanely-expensive" && page !== "https://dekernmetjordi.com/guitar-solos-are-still-awesome" && page !== "https://dekernmetjordi.com/live-music-is-crucial" && page !== "https://dekernmetjordi.com/making-a-home-studio" && page !== "https://dekernmetjordi.com/creating-a-successful-music-brand" && page !== "https://dekernmetjordi.com/bammm" && page !== "https://dekernmetjordi.com/behind-the-scenes-with-our-artists" && page !== "https://dekernmetjordi.com/blog" && page !== "https://dekernmetjordi.com/blog/1" && page !== "https://dekernmetjordi.com/blog/2" && page !== "https://dekernmetjordi.com/blog/3" && page !== "https://dekernmetjordi.com/category/career%20advice" && page !== "https://dekernmetjordi.com/category/insights" && page !== "https://dekernmetjordi.com/category/latest" && page !== "https://dekernmetjordi.com/category/persoonlijk" && page !== "https://dekernmetjordi.com/category/reiki" && page !== "https://dekernmetjordi.com/category/something%20cool" && page !== "https://dekernmetjordi.com/category/studio%20tips" && page !== "https://dekernmetjordi.com/collaboration-in-music-production"
-  }), partytown(), 
+  }), partytown()
   // compress()
-],
-  site: "https://dekernmetjordi.com"
+  ],
+  site: "https://dekernmetjordi.com",
+  output: "hybrid",
+  adapter: netlify()
 });
---
+++ src/pages/api/emailForm.json.ts
@@ -0,0 +1,60 @@
+import type { APIRoute } from "astro";
+import MailerLite from "@mailerlite/mailerlite-nodejs";
+
+// Dit is ervoor om ervoor te zorgen dat dit gedeelte SSR is inplaats van MPA
+export const prerender = false;
+
+export const POST: APIRoute = async ({ request }) => {
+  const key = import.meta.env.API_TOKEN;
+  console.log("De key is: " + key);
+  const data = await request.json();
+  console.log(data);
+
+  const mailerlite = new MailerLite({
+    api_key: key,
+  });
+
+  const params = {
+    email: data.email,
+    groups: ["118882712635311623"],
+    status: "active", // possible statuses: active, unsubscribed, unconfirmed, bounced or junk.
+  };
+
+  console.log(params);
+
+  mailerlite.subscribers
+    .createOrUpdate(params)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      if (error.response) console.log(error.response.data);
+    });
+
+  // const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //   method: "POST",
+  //   body: JSON.stringify(data),
+  //   headers: {
+  //     "Content-type": "application/json; charset=UTF-8",
+  //     // "Accept": "application/json",
+  //     Authentication: `Bearer ${key}`,
+  //   },
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      // "Accept": "application/json",
+      Authentication: `Bearer ${key}`,
+    },
+  });
+
+  const output = await res.json();
+  console.log("output is: " + JSON.stringify(output));
+  return new Response(
+    JSON.stringify({
+      message: output.id ? "success" : "failure",
+    })
+  );
+};
