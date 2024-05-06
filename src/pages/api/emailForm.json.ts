516b7909601284c493daa43c45e66330f2a081b5
Dekernmetjordi <info@dekernmetjordi.com>
2024-05-06 10:14:26 +0200

    nieuwsbrief toegevoegd

--- src/pages/api/emailForm.json.ts
+++ src/pages/api/emailForm.json.ts
@@ -4,8 +4,11 @@ import MailerLite from "@mailerlite/mailerlite-nodejs";
 // Dit is ervoor om ervoor te zorgen dat dit gedeelte SSR is inplaats van MPA
 export const prerender = false;
 
+const API_TOKEN="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiY2ZmMWU5ZDY2OWI1ZmVmNzA4YzM3ZmYxZWQ5YTY0ZTUxMmM5YmMyOTU4ZDE1MjYxZTcyYTMyMzJhMGZiZDlhMTA2ZGNlYjc0ZDA5MDUzNDEiLCJpYXQiOjE3MTQzMjcwOTcuNjI2MjMyLCJuYmYiOjE3MTQzMjcwOTcuNjI2MjM0LCJleHAiOjQ4NzAwMDA2OTcuNjIyNzAxLCJzdWIiOiI5MjI5NjAiLCJzY29wZXMiOltdfQ.vTm07HVG9WHvh8BDwDzyF8DR2ROjo8HintYIOfwF3BntIxLFmumCVcxx64qUfl6O0g5mFevy-iKCrzp6GodU6-RIlIvhX05TCReNhPzMkneOwhfL_AJFGtekZTCjd-wj83C8J3ztMMaTrx2xwKMmRi74YgESXuUzqSz1rEX4KaKz5_iR0FGqQRPMdHqtbXfZrC1mLa9GDgVLyuRw9bKNd1cTyl-Y6RtS5snlMNILJojpV24EKgeHxzRxTnZuimP_8gnLh5IYzZahVDhvi42ceLz9zstUSiVU31Sk-0qNlVnKYgnw4usxTgXGThyiXvTVeKW_UI2t39HgDpcwCqFB8chNf1thVhS-R5lALnqRu8EFLnETAGyyUGjdvBJFYSwgj-jlUOXRTXW7sLcOb4uFj0CjoOrT4c0VbLMUsNXYDl8Tw5CYnENI6c6zCNILQC8kGCkZbYiaaqOTA8Bz-UbT-1-jkzoO613X7vWpYgYR43_61gE2VjPuc5PsJvXZVtt8Vn_SttrY2hp4a40PKkLIioWV_Qj_6vtSVTTYpqPoZCNVmvgK3Tb0JHormQQY70wrqAHDIFXQI3VT74dFq-832cLgIRQUCG-DOpcgPz9ybDLix9zymLu63Eb3onp3s3dUdSC6-Q1xkJXmLTrIPCrEOxiur6tBH4MqcREb-GZVcBA"
+
 export const POST: APIRoute = async ({ request }) => {
-  const key = import.meta.env.API_TOKEN;
+  // const key = import.meta.env.API_TOKEN;
+  const key = API_TOKEN;
   // console.log("De key is: " + key);
   const data = await request.json();
   console.log(data);
